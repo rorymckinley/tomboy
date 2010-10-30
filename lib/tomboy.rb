bf2648cb0d90ed3b88e924e66ff5363e2460642b
Rory McKinley <rorymckinley@gmail.com>
2010-10-30 20:34:08 +0200

    Very basic write functionality - next step, expanding it so that it build the complete xml

--- lib/tomboy.rb
+++ lib/tomboy.rb
@@ -12,6 +12,23 @@ class Tomboy
     @note_obj = note_obj
   end
   def content
-    @note_obj.css('note-content').text
+    @note_obj.xpath('//bns:note-content', { "bns" => "http://beatniksoftware.com/tomboy"}).text
+  end
+  def self.write(note_content, note_path)
+    xml_builder = seed_template_with note_content
+    File.open(note_path, 'w') do |file|
+      file.write(xml_builder.to_xml)
+    end
+    new(xml_builder.doc)
+  end
+
+  private
+
+  def self.seed_template_with(note_content)
+    Nokogiri::XML::Builder.new(:encoding => 'utf-8') do |xml|
+      xml.note( 'version' => "0.3", 'xmlns:link' => "http://beatniksoftware.com/tomboy/link", 'xmlns:size'=> "http://beatniksoftware.com/tomboy/size", 'xmlns' => "http://beatniksoftware.com/tomboy") do
+        xml.send("note-content".to_sym, note_content, 'version' => "0.1")
+      end
+    end
   end
 end
--- spec/tomboy_spec.rb
+++ spec/tomboy_spec.rb
@@ -2,13 +2,33 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'rubygems'
 
 describe "Reading a Tomboy note file" do
-  before(:all) do
+  before(:each) do
     @note_path = File.join(File.dirname(__FILE__), 'fixtures', 'read_tomboy_note.note')
   end
+
   it "should instantiate itself based on an existing Tomboy note file" do
     Tomboy.read(@note_path).should be_an_instance_of Tomboy
   end
   it "should instantiate itself and contain the content of the tomboy note" do
     Tomboy.read(@note_path).content.should == 'This is my fixture content'
+  end
+end
+
+describe "Writing to a Tomboy note file" do
+  before(:each) do
+    @benchmark_path = File.join(File.dirname(__FILE__), 'fixtures', 'benchmark.note')
+    @written_note_path = '/tmp/written_tomboy_note.note'
+    File.delete if File.exist? @written_note_path
+  end
+
+  it "should create a tomboy note from a simple string" do
+    t = Tomboy.write('This is my fixture content', @written_note_path)
+    t.should be_an_instance_of Tomboy
+    t.content.should == 'This is my fixture content'
+  end
+
+  it "should create the note in the location specified" do
+    Tomboy.write('This is my fixture content', @written_note_path)
+    File.exist?(@written_note_path).should be_true
   end
 end
