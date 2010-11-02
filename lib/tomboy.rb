33c58be708fed38aff59737fb137cede2e937dd7
Rory McKinley <rorymckinley@gmail.com>
2010-11-02 21:19:30 +0200

    XML schema done plus some stubbed values in the DB

--- lib/tomboy.rb
+++ lib/tomboy.rb
@@ -27,9 +27,19 @@ class Tomboy
   def self.seed_template_with(note_content)
     Nokogiri::XML::Builder.new(:encoding => 'utf-8') do |xml|
       xml.note( 'version' => "0.3", 'xmlns:link' => "http://beatniksoftware.com/tomboy/link", 'xmlns:size'=> "http://beatniksoftware.com/tomboy/size", 'xmlns' => "http://beatniksoftware.com/tomboy") do
+        xml.title 'This is my title'
         xml.text_('xml:space' => 'preserve') do
           xml.send("note-content".to_sym, note_content, 'version' => "0.1")
         end
+        xml.send("last-change-date", "2008-08-28T17:18:04.4700640-07:00")
+        xml.send("last-metadata-change-date", "2008-08-28T17:18:04.4700640-07:00")
+        xml.send("create-date", "2008-08-28T17:18:04.4700640-07:00")
+        xml.send("cursor-position".to_sym, 0)
+        xml.width 450
+        xml.height 360
+        xml.x 0
+        xml.y 0
+        xml.send("open-on-startup".to_sym, "False")
       end
     end
   end
